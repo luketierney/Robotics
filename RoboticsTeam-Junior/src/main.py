11e398dcc262bb97f2cbc6d59c18a2d2825d270b
Luke <luketierney06@gmail.com>
2023-01-19 14:18:45 -0500

    Robotics Program

---
+++ RoboticsTeam-Junior/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       luketierney                                                  #
+# 	Created:      1/19/2023, 2:17:18 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
